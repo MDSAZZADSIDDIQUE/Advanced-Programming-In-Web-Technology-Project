1c9f6e28f837f955794bca65d7d9406e79e74e4c
MDSAZZADSIDDIQUE <mdsazzadsiddique0@gmail.com>
2023-06-16 02:50:10 +0600

    Edit Profile Details Add

--- nature-nurtures-mid-term-project/src/Member/member.service.ts
+++ nature-nurtures-mid-term-project/src/Member/member.service.ts
@@ -1,27 +1,46 @@
 import { Injectable } from "@nestjs/common";
-import { MemberDTO } from "./member.dto";
+import { EditMemberDTO, MemberDTO } from "./member.dto";
 
 @Injectable()
 export class MemberService{
     getProfileDetails(profileDetails: MemberDTO): string {
-        const firstName = profileDetails.firstName;
-        const lastName = profileDetails.lastName;
-        const dateOfBirth = profileDetails.dateOfBirth;
-        const address = profileDetails.address;
-        const username = profileDetails.username;
-        const telephoneNumber = profileDetails.telephoneNumber;
-        const email = profileDetails.email;
-        const password = profileDetails.password;
+        const now = new Date();
+        const milliseconds = now.getTime();
+        const id = milliseconds.toString();
         return (`
         Profile Details
         ---------------
-        Name: ${firstName} ${lastName}
-        Date of Birth: ${dateOfBirth}
-        Address: ${address}
-        Username: ${username}
-        Telephone Number: ${telephoneNumber}
-        Email: ${email}
+        ID: ${id}
+        Name: ${profileDetails.firstName} ${profileDetails.lastName}
+        Date of Birth: ${profileDetails.dateOfBirth}
+        Address: ${profileDetails.address}
+        Username: ${profileDetails.username}
+        Telephone Number: ${profileDetails.telephoneNumber}
+        Email: ${profileDetails.email}
         ---------------
         `);
     }
+
+    editProfileDetails(query: EditMemberDTO, profileDetails: MemberDTO)
+    {
+        const editKey = query.editKey;
+        const ediValue = query.editValue;
+        for (const key in profileDetails) {
+            if (key === editKey) {
+                profileDetails[key] = ediValue;
+            }
+        }
+        return (`
+        Profile Details
+        ---------------
+        Name: ${profileDetails.firstName} ${profileDetails.lastName}
+        Date of Birth: ${profileDetails.dateOfBirth}
+        Address: ${profileDetails.address}
+        Username: ${profileDetails.username}
+        Telephone Number: ${profileDetails.telephoneNumber}
+        Email: ${profileDetails.email}
+        ---------------
+        `);
+    }
+    
 }
