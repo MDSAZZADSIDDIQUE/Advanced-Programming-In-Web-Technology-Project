20bb29e1a0a764139dcaff6625c6f689618706bc
Shafin_ahmed <Ahmedshafin112233@gmail.com>
2023-06-17 19:50:48 +0600

    Initial Commit from Shafin

--- nature-nurtures-mid-term-project/src/Admin/admin/admin.service.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
-@Injectable()
-export class AdminService {
-  getAdmin(): string {
-    return 'hello shafin';
-  }
-}
